377c1e2b6708d376314f851171be22ab48d28fd7
dac392 <dac392@scarletmail.rutgers.edu>
2022-09-04 16:05:10 -0400

    UpdateCourse

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
 import React, { useContext, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import AllUsers from './components/AllUsers';
+// import AllUsers from './components/AllUsers';
 import Header from './components/Header';
 import Courses from './components/Courses';
 import LogIn from './components/LogIn';
 import PrivateRoute from './PrivateRoute';
 import CreateCourses from './components/CreateCourses';
 import CourseDetails from './components/CourseDetails';
+import UpdateCourse from './components/UpdateCourse';
 
 const App = () => {
-
-  // const { isLoading , actions } = useContext(Context);
-  // useEffect( ()=>{
-  //   actions.getAllUsers()
-  //     .then(response => actions.setAllUsers(response))
-  //     .catch(error => console.log(error.messgage))
-  //     .finally( ()=>actions.setIsLoading(false))
-  // }, []);
-
   return (
     <div>
       <Header />
@@ -27,17 +19,11 @@ const App = () => {
         <Route exact path="/" element={ <Courses /> }/>
         <Route path="/courses/" element={ <PrivateRoute /> }>
           <Route path="create" element={ <CreateCourses /> } />
-          <Route path=":id/update" element={ <h1>Something</h1> }/>
+          <Route path=":id/update" element={ <UpdateCourse /> }/>
           <Route path=":id" element={ <CourseDetails /> } />
         </Route>
         <Route path="/login" element={ <LogIn /> } />
       </Routes>
-
-      {/* {
-        isLoading
-        ? <p>Loading...</p>
-        : <AllUsers />
-      } */}
     </div>
   );
 }
---
+++ client/src/components/UpdateCourse.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const UpdateCourse = ()=>{
+    return (
+        <h1>Update Screen</h1>
+    );
+}
+
+export default UpdateCourse;
